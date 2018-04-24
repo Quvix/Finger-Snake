cb9cb374b5caee82ee557e0347490e42b6a89a41
Jakub Vitásek <quvixcz@gmail.com>
2018-04-25 00:55:40 +0200

    wip

--- js/sketch3.js
+++ js/sketch3.js
@@ -1,27 +1,52 @@
 const debug = true;
+const GET_HIGHSCORE_URL = 'http://highscore.quvix.eu/api/gethighscores';
+const ADD_HIGHSCORE_URL = 'http://highscore.quvix.eu/api/addhighscore';
+const HASH_KEY = 'rF1eNtMvadnpPUXT64OiVlyqDzEfBoH0';
+
+let config = {
+    username: 'UNKNOWN',
+    color: {
+        R: 255,
+        G: 255,
+        B: 255
+    }
+};
 
 let rotateMobileImg;
-let rotateAngle = 0;
 let gsm = new GameStateManager();
-let soundtrack;
+let soundtrack_cowboy;
+let soundtrack_cowboy_underwater;
 
 addEventListener("click", function() {
     if(!isFullscreen()) {
         enableFullscreen();
-        gsm.changeState('menu', true);
     }
 });
+
+function saveConfig() {
+    localStorage['config'] = JSON.stringify(config);
+}
+
+function loadConfig() {
+    data = JSON.parse(localStorage.getItem('config'));
+    if(data !== null) {
+        config = data;
+    }
+}
 
 function preload() {
     soundFormats('mp3', 'ogg');
     bite = loadSound('sounds/bite.mp3');
     rotateMobileImg = loadImage('images/rotate.png');
-    soundtrack = loadSound('sounds/hp.mp3');
+    soundtrack_cowboy = loadSound('sounds/cowboy.mp3');
+    soundtrack_cowboy_underwater = loadSound('sounds/cowboy-underwater.mp3');
+    loadConfig();
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     gsm.init();
+    gsm.changeState('menu', true);
 }
 
 function windowResized() {
@@ -39,6 +64,7 @@ function draw() {
                 textSize(32);
                 textAlign(CENTER);
                 fill(255);
+                textSize(32);
                 text('PRESS TO ENABLE FULLSCREEN', windowWidth / 2, windowHeight / 2);
             }
         } else {
@@ -59,54 +85,55 @@ function draw() {
 
 // Snake class
 function Snake() {
-    this.x = [];
-    this.y = [];
-    this.segNum = 10;
-    this.segLength = 18;
+    let segNum = 100;
+    let segLength = 4;
+    let thickness = 10;
+    let x = [];
+    let y = [];
 
-    for (let i = 0; i < this.segNum; i++) {
-        this.x[i] = windowWidth / 2;
-        this.y[i] = windowHeight / 2;
+    for (let i = 0; i < segNum; i++) {
+        x[i] = windowWidth / 2;
+        y[i] = windowHeight / 2;
     }
 
     this.draw = function() {
         push();
-        strokeWeight(9);
-        stroke(255, 100);
+        strokeWeight(thickness);
+        stroke(config.color.R, config.color.G, config.color.B, 100);
         this.dragSegment(0, mouseX, mouseY);
-        for(let i=0; i < this.x.length - 1; i++) {
-            this.dragSegment(i + 1, this.x[i], this.y[i]);
+        for(let i=0; i < x.length - 1; i++) {
+            this.dragSegment(i + 1, x[i], y[i]);
         }
         pop();
     };
 
     this.dragSegment = function(i, xin, yin) {
-        let dx = xin - this.x[i];
-        let dy = yin - this.y[i];
+        let dx = xin - x[i];
+        let dy = yin - y[i];
         let angle = atan2(dy, dx);
-        this.x[i] = xin - cos(angle) * this.segLength;
-        this.y[i] = yin - sin(angle) * this.segLength;
-        this.drawSegment(this.x[i], this.y[i], angle);
+        x[i] = xin - cos(angle) * segLength;
+        y[i] = yin - sin(angle) * segLength;
+        this.drawSegment(x[i], y[i], angle);
     };
 
     this.drawSegment = function(x, y, a) {
         push();
         translate(x, y);
         rotate(a);
-        line(0, 0, this.segLength, 0);
+        line(0, 0, segLength, 0);
         pop();
     };
 
     this.incSegNum = function(count = 1) {
         for(let i = 1; i <= count; i++) {
-            this.x[this.segNum + i - 1] = this.x[this.segNum + i - 2];
-            this.y[this.segNum + i - 1] = this.y[this.segNum + i - 2];
+            x[segNum + i - 1] = x[segNum + i - 2];
+            y[segNum + i - 1] = y[segNum + i - 2];
         }
-        this.segNum += count;
+        segNum += count;
     };
 
     this.incSegLength = function(length = 1) {
-        this.segLength += length;
+        segLength += length;
     };
 }
 
@@ -134,6 +161,8 @@ function GameStateManager() {
     this.init = function() {
         this.states['play'] = new PlayState();
         this.states['menu'] = new MenuState();
+        this.states['menuSettings'] = new MenuSettingsState();
+        this.states['deathState'] = new DeathState();
     };
 
     this.draw = function() {
@@ -143,33 +172,29 @@ function GameStateManager() {
         }
     };
 
-    this.changeState = function(state, init = false) {
+    this.changeState = function(state, init = false, params = {}) {
         this.activeState = state;
         if(init) {
-            this.states[this.activeState].init();
+            this.states[this.activeState].init(params);
         }
     }
 }
 
 function MenuState() {
-    this.menu = new Menu();
+    this.init = function(params) {
+        this.menu = new Menu();
 
-    let btn1 = new MenuButton();
-    btn1.text = "START GAME";
-    btn1.action = function() {
-        gsm.changeState('play', true);
-    };
-    this.menu.addButton(btn1);
+        this.menu.addButton(
+            new MenuButton("START GAME", function() {
+                gsm.changeState('play', true);
+            })
+        );
 
-    let btn2 = new MenuButton();
-    btn2.text = "DAVID JE MOCKAAAAAAAA";
-    btn2.action = function() {
-        gsm.changeState('menu', true);
-    };
-    this.menu.addButton(btn2);
-
-    this.init = function() {
-
+        this.menu.addButton(
+            new MenuButton("SETTINGS", function() {
+                gsm.changeState('menuSettings', true);
+            })
+        );
     };
 
     this.draw = function() {
@@ -187,31 +212,141 @@ function MenuState() {
     }
 }
 
-function PlayState() {
-    this.snake;
+function MenuSettingsState() {
+    this.init = function(params) {
+        this.menu = new Menu();
 
-    this.init = function() {
-        this.snake = new Snake();
-        //soundtrack.play();
+        this.menu.addButton(
+            new MenuButton('USERNAME', function() {
+                let input = prompt("Please enter your name:", config.username);
+                if (input != null && input !== "") {
+                    config.username = input;
+                }
+            })
+        );
+        this.menu.addButton(
+            new MenuButton("BACK", function() {
+                saveConfig();
+                gsm.changeState('menu', true);
+            })
+        );
     };
 
     this.draw = function() {
-        if(mouseIsPressed) {
+        this.menu.draw();
+    }
+}
+
+function PlayState() {
+    let snake;
+    let started;
+    let fingerReleased;
+    let score = 0;
+    let startTime = performance.now();
+
+    this.init = function(params) {
+        snake = new Snake();
+        started = false;
+        fingerReleased = false;
+    };
+
+    this.draw = function() {
+        if(started) {
+            if(!mouseIsPressed) {
+                soundtrack_cowboy.stop();
+                gsm.changeState('deathState', true, {'score': score, 'duration': performance.now() - startTime});
+            }
+
             background(0);
-            this.snake.draw();
+            snake.draw();
+            textAlign(CENTER);
+            textSize(32);
+            text(score, windowWidth / 2, 50);
+
         } else {
-            gsm.changeState('menu', true);
+            if(mouseIsPressed) {
+                if(fingerReleased) {
+                    if(collidePointCircle(mouseX, mouseY, windowWidth / 2, windowHeight / 2, 50)) {
+                        started = true;
+                        soundtrack_cowboy.play();
+                        soundtrack_cowboy.setVolume(0.5);
+                    }
+                }
+            } else {
+                if(!fingerReleased) {
+                    fingerReleased = true;
+                }
+            }
+            this.drawInstructionsBeforeGame();
         }
+    };
+
+    this.drawInstructionsBeforeGame = function() {
+        background(0);
+        fill(255);
+        ellipse(windowWidth / 2, windowHeight / 2, 50, 50);
+        textAlign(CENTER);
+        textSize(32);
+        text("TO START PUT FINGER HERE", windowWidth / 2, windowHeight / 2 + 100);
+    };
+}
+
+function DeathState() {
+    const TIME_PER_SCORE = 10000;
+    let score;
+    let duration;
+    let finalScore;
+    let rank = 'LOADING';
+
+    this.init = function(params) {
+        soundtrack_cowboy_underwater.play();
+        score = params.score;
+        duration = params.duration;
+        if(score !== 0) {
+            finalScore = Math.round(TIME_PER_SCORE / (duration / score) * 100) / 100;
+
+            let jqxhr = $.ajax({
+                url: ADD_HIGHSCORE_URL,
+                type: "GET",
+                data: {
+                    hash_key: HASH_KEY,
+                    name: config.username,
+                    score: finalScore
+                },
+                success: function(data) {
+                    console.log(data);
+                    rank = data.rank;
+                }
+            })
+        } else {
+            finalScore = 0;
+            rank = 'OUT OF RANGE :(';
+        }
+
+    };
+
+    this.draw = function() {
+        background(0);
+        textAlign(CENTER);
+        textSize(96);
+        fill(255);
+        text(finalScore, windowWidth / 2, 200);
+        textSize(32);
+        text('RANK: ' + rank, windowWidth / 2, 350);
     }
 }
 
 function Menu() {
     let buttons = [];
     let level = 1;
+    let released = false;
 
     this.draw = function() {
+        if(!mouseIsPressed) {
+            released = true;
+        }
         for(let i = 0; i < buttons.length; i++) {
-            buttons[i].draw();
+            buttons[i].draw(this.isClicked());
         }
     };
 
@@ -219,22 +354,33 @@ function Menu() {
         btn.level = level;
         buttons.push(btn);
         level++;
-    }
+    };
+
+    this.isClicked = function() {
+        if(mouseIsPressed) {
+            if(released) {
+                return true;
+            }
+        }
+        return false;
+    };
 
 }
 
-function MenuButton() {
-    const WIDTH = windowWidth * 0.8;
+function MenuButton(desc, action) {
+    const WIDTH = windowWidth * 0.9;
     const HEIGHT = 50;
     const SPACE_HEIGHT = 100;
     const START_HEIGHT = 100;
     const FONT_SIZE = 32;
-    this.text = "UNDEFINED";
+    this.text = desc;
     this.level = -1;
+    this.action = action;
 
-    this.draw = function() {
+    this.draw = function(clicked) {
         let r = this.getRect();
-        if(mouseIsPressed && collidePointRect(mouseX, mouseY, r.x, r.y, r.width, r.height)) {
+        if(clicked && collidePointRect(mouseX, mouseY, r.x, r.y, r.width, r.height)) {
+            console.log("???");
             this.action();
         } else {
             fill(0);
